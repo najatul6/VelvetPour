3dbb3e1085393f476037df310dca9e3bc971b329
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-05-15 05:53:59 +0600

    Adjust import order in src/main.jsx
    Move the './index.css' import below the Routes import and add a separating newline for consistent import grouping and formatting. This is a non-functional change to improve code organization.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router'
 import Routes from './router/Routes'
+import './index.css'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={Routes}></RouterProvider>
